5b4db31e3608fd57d0cd92ef06db7e9d14e7b1f9
track-frags <jadirks@anamosa.k12.ia.us>
2025-11-06 18:10:36 +0000

    save

--- test/verify.test.js
+++ test/verify.test.js
@@ -34,8 +34,12 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  if (browser) {
+    await browser.close();
+    browser = null;
+  }
 });
+
 
 describe('the board class', () => {
   it('should display the kanban columns vertically when the screen is a maximum width of 400px', async () => {
